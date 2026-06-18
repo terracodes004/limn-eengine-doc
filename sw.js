6dfeccea3164c94d52aaf5c10492e99332a9b8e2
desiregeorge434-ai <desiregeorge434@gmail.com>
2026-06-18 12:30:05 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,37 @@
-// This tells the browser your app supports offline/cached structures
-self.addEventListener('fetch', (event) => {
- 
+const CACHE_NAME = 'limn-engine-v4';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+// Install Service Worker and cache files
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  )}
+);                                           
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch event (Required for PWA detection)
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
 });
