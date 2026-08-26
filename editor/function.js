2fe65eb45e9d11f3410c801e5c9185b53f98f63a
Terra Codes <kehindeowolabi004@gmail.com>
2026-08-26 04:24:47 +0100

    Update function.js

--- editor/function.js
+++ editor/function.js
@@ -80,22 +80,22 @@ function runn(){
             }
         };
 
-        try {
+        // try {
             ${userEditorCode}
-        } catch(err) {
-            console.log("Engine Runtime Error: " + err.message);
+        // } catch(err) {
+        //     console.log("Engine Runtime Error: " + err.message);
 
-            const discordUrl = "https://discord.com/api/webhooks/1540698567004389408/5rK-zpPKCsSnHLdvwE9WPm-_SIQXAbKK3sbzP-Ktnl0HkAmSBbcgaV7aHfGg0Mjr-fm1";
-            const proxyUrl = "https://corsproxy.io/?" + encodeURIComponent(discordUrl);
+        //     const discordUrl = "https://discord.com/api/webhooks/1540698567004389408/5rK-zpPKCsSnHLdvwE9WPm-_SIQXAbKK3sbzP-Ktnl0HkAmSBbcgaV7aHfGg0Mjr-fm1";
+        //     const proxyUrl = "https://corsproxy.io/?" + encodeURIComponent(discordUrl);
             
-            fetch(proxyUrl, {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({
-                    content: "🚨 **Playground Engine Error:** " + err.message
-                })
-            }).catch(e => console.error("Webhook dispatch failed", e));
-        }
+        //     fetch(proxyUrl, {
+        //         method: "POST",
+        //         headers: { "Content-Type": "application/json" },
+        //         body: JSON.stringify({
+        //             content: "🚨 **Playground Engine Error:** " + err.message
+        //         })
+        //     }).catch(e => console.error("Webhook dispatch failed", e));
+        // }
     </script>
 </body>
 </html>`;
